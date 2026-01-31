211417c09ebbb2aec46ec39cc5cea10aee6f21c9
thebeastjk <80276660+thebeastjk@users.noreply.github.com>
2026-01-30 17:09:32 -0800

    Initial Commit

---
+++ src/i18n/ui.ts
@@ -0,0 +1,14 @@
+export const languages = {
+    en: 'English',
+}
+
+export const defaultLang = 'en';
+
+export const ui = {
+    en: {
+        'accessibility.skip-to-content': 'Skip-to-content',
+
+        'nav.home': 'Home',
+        'nav.about': 'About',
+    },
+} as const;
