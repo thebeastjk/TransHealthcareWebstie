e3ff5f5d3ff5f53e3d3a8101c0223c8ba303b384
thebeastjk <80276660+thebeastjk@users.noreply.github.com>
2026-02-03 19:01:05 -0800

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,8 @@ import { defineConfig, fontProviders } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-    
+    site: 'https://thebeastjk.github.io/',
+    base: '/TransHealthcareWebstie',
     scopedStyleStrategy: 'where',
     experimental: {
         fonts: [
