5e21399e5bf839755a023815415b0c69618de680
thebeastjk <80276660+thebeastjk@users.noreply.github.com>
2026-02-17 09:58:18 -0800

    Change to inlinestyesheets
    aka making the animation actually act as it does on localhost, there's probably a better way to do this but tbh I can't be fucked.

--- astro.config.mjs
+++ astro.config.mjs
@@ -9,6 +9,9 @@ export default defineConfig({
     vite: {
         plugins: [tailwindcss()],
     },
+    build: {
+        inlineStylesheets: 'always',
+    },
     scopedStyleStrategy: 'where',
     experimental: {
         fonts: [
